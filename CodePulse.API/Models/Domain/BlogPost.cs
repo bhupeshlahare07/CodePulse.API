461549efe996803b659f7986e0cddd437af44f7e
bhupeshlahare <bhupeshlahare@gmail.com>
2024-08-24 12:34:22 +0530

    Adding Domain Model

---
+++ CodePulse.API/Models/Domain/BlogPost.cs
@@ -0,0 +1,14 @@
+﻿namespace CodePulse.API.Models.Domain
+{
+	public class BlogPost
+	{
+        public Guid Id { get; set; }
+		public string Title {  get; set; }
+		public string ShortDescription { get; set; }
+		public string FeaturedImageUrl { get; set; }
+		public string UrlHandel { get; set; }
+		public DateTime? PublishedDate { get; set; }
+		public string Author {  get; set; }
+		public bool Isvisible {  get; set; }
+    }
+}
