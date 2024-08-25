36ff7a69527a672c0be22f72b75e5e0af86306fc
bhupeshlahare <bhupeshlahare@gmail.com>
2024-08-25 18:14:23 +0530

    Adding DBContext class

---
+++ CodePulse.API/Data/ApplicationDBContext.cs
@@ -0,0 +1,16 @@
+﻿using CodePulse.API.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+using System.Data.Common;
+
+namespace CodePulse.API.Data
+{
+	public class ApplicationDBContext : DbContext
+	{
+		public ApplicationDBContext(DbContextOptions options) : base(options)
+		{
+		}
+
+		public DbSet<BlogPost> BlogPosts { get; set; }
+		public DbSet<Category> Categories { get; set; }
+	}
+}
