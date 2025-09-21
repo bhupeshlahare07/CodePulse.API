04ec7b488de2fa4894c473f7e9676e5c5c76a31e
Bhupesh Lahare <bhupeshlahare@gmail.com>
2025-09-21 18:45:24 +0530

    Get all blog post

--- CodePulse.API/Controllers/BlogPostController.cs
+++ CodePulse.API/Controllers/BlogPostController.cs
@@ -8,45 +8,66 @@ namespace CodePulse.API.Controllers
     [Route("api/[controller]")]
     [ApiController]
     public class BlogPostController : Controller
-	{
-		private readonly IBlogPostRepository blogPostRepository;
+    {
+        private readonly IBlogPostRepository blogPostRepository;
 
-		public BlogPostController(IBlogPostRepository blogPostRepository)
-		{
-			this.blogPostRepository = blogPostRepository;
-		}
+        public BlogPostController(IBlogPostRepository blogPostRepository)
+        {
+            this.blogPostRepository = blogPostRepository;
+        }
 
-		[HttpPost]
-        public async Task<IActionResult> CreateBlogPost([FromBody]CreateBlogPostRequestDto request)
-		{
-			//Convert Dto to Domain Model
-			var blogPost = new BlogPost
-			{
-				Title = request.Title,
-				ShortDescription = request.ShortDescription,
-				Content = request.Content,
-				UrlHandle = request.UrlHandle,
-				PublishedDate = request.PublishedDate,
-				Author = request.Author,
-				FeaturedImageUrl = request.FeaturedImageUrl,
+        [HttpPost]
+        public async Task<IActionResult> CreateBlogPost([FromBody] CreateBlogPostRequestDto request)
+        {
+            //Convert Dto to Domain Model
+            var blogPost = new BlogPost
+            {
+                Title = request.Title,
+                ShortDescription = request.ShortDescription,
+                Content = request.Content,
+                UrlHandle = request.UrlHandle,
+                PublishedDate = request.PublishedDate,
+                Author = request.Author,
+                FeaturedImageUrl = request.FeaturedImageUrl,
                 Isvisible = request.Isvisible
-			};
-			blogPost = await blogPostRepository.CreateAsync(blogPost);
-
-			//Convert Domain Model to Dto
-			var response = new BlogPostDto
-			{
-				Id = blogPost.Id,
-				Title = blogPost.Title,
-				ShortDescription = blogPost.ShortDescription,
-				Content = blogPost.Content,
-				UrlHandle = blogPost.UrlHandle,
-				PublishedDate = blogPost.PublishedDate,
-				Author = blogPost.Author,
-				FeaturedImageUrl = blogPost.FeaturedImageUrl,
-				Isvisible = blogPost.Isvisible
             };
-			return Ok(response);
+            blogPost = await blogPostRepository.CreateAsync(blogPost);
+
+            //Convert Domain Model to Dto
+            var response = new BlogPostDto
+            {
+                Id = blogPost.Id,
+                Title = blogPost.Title,
+                ShortDescription = blogPost.ShortDescription,
+                Content = blogPost.Content,
+                UrlHandle = blogPost.UrlHandle,
+                PublishedDate = blogPost.PublishedDate,
+                Author = blogPost.Author,
+                FeaturedImageUrl = blogPost.FeaturedImageUrl,
+                Isvisible = blogPost.Isvisible
+            };
+            return Ok(response);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllBlogPost()
+        {
+            var blogPosts = await blogPostRepository.GetAllAsync();
+
+            var response = blogPosts.Select(blogPost => new BlogPostDto
+            {
+                Id = blogPost.Id,
+                Title = blogPost.Title,
+                ShortDescription = blogPost.ShortDescription,
+                Content = blogPost.Content,
+                UrlHandle = blogPost.UrlHandle,
+                PublishedDate = blogPost.PublishedDate,
+                Author = blogPost.Author,
+                FeaturedImageUrl = blogPost.FeaturedImageUrl,
+                Isvisible = blogPost.Isvisible
+            }).ToList();
+
+            return Ok(response);
         }
     }
 }
