8eb75587ad9c276b268804107c293416524f6eb9
Bhupesh Lahare <bhupeshlahare@gmail.com>
2025-09-21 17:03:26 +0530

    Create blog post model corrections

---
+++ CodePulse.API/Migrations/20250921034519_RenameUrlHandelToUrlHandle.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CodePulse.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenameUrlHandelToUrlHandle : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "UrlHandel",
+                table: "BlogPosts",
+                newName: "UrlHandle");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Content",
+                table: "BlogPosts",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Content",
+                table: "BlogPosts");
+
+            migrationBuilder.RenameColumn(
+                name: "UrlHandle",
+                table: "BlogPosts",
+                newName: "UrlHandel");
+        }
+    }
+}
